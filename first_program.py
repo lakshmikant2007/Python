209f349d062f7b1f5d9e313f817dbe987b2fba51
Lakshmikant Kumar <lakshmikantkumar22@gmail.com>
2026-03-17 16:11:01 +0530

    Add files via upload

--- first_program.py
+++ first_program.py
@@ -1,1 +1,18 @@
-print("hello world")
+import mysql.connector
+
+# Connect to MySQL
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root"
+)
+
+cursor = conn.cursor()
+
+# Create Database
+cursor.execute("CREATE DATABASE SELECTION_DB")
+
+print("Database created successfully")
+
+# Close connection
+conn.close()
