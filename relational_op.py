b7d278e0493c2eaa2ad2e82db6363a059b765734
Lakshmikant Kumar <lakshmikantkumar22@gmail.com>
2026-03-19 13:52:02 +0530

    Add files via upload

---
+++ relational_op.py
@@ -0,0 +1,11 @@
+#Relational/comparison op( ==, !=, <, >, >=, <= )
+
+a=50
+b=20
+
+print(a == b) #False
+print(a != b) #True
+print(a < b)  #False
+print(a > b)  #True
+print(a <= b) #False 
+print(a >= b) #True
