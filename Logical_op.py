b6c331eb81e2a506416b94009d8a162f20f7a3c6
Lakshmikant Kumar <lakshmikantkumar22@gmail.com>
2026-03-19 13:52:34 +0530

    Add files via upload

---
+++ Logical_op.py
@@ -0,0 +1,21 @@
+#Logical op( not, and, or )
+#not [boolean oposite value]
+
+"""
+a = 40
+b = 30
+print(not False)  #True
+print(not ( a > b))  #False
+
+"""
+#and [if two value with True its True otherwise False]
+a = 50
+b = 30
+  
+val1 = True 
+val2 = True
+print("and operator:",val1 and val2) #True
+
+#or [if any one value is True its True, ow False]
+
+print("or oprator",val1 or val2) #True
