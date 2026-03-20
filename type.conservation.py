8b3bb5d63446e11fbc64e1580a53bedb567f9b54
Lakshmikant Kumar <lakshmikantkumar22@gmail.com>
2026-03-20 14:00:21 +0530

    Add files via upload

---
+++ type.conservation.py
@@ -0,0 +1,15 @@
+#type conservation and type casting
+'''
+a = 2
+b = 4.53   #[There's b is float but it change the val of a is in float ]
+
+sum = a + b
+print(sum)
+'''
+
+#type casting
+
+a = int ("25")  #There he changes the value by casting the int
+b = 50.38
+
+print(a+b)
