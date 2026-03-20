48e27eb5f819e95d6c74ce1101c7d763b096a0f1
Lakshmikant Kumar <lakshmikantkumar22@gmail.com>
2026-03-20 14:00:36 +0530

    Add files via upload

---
+++ input.in.py
@@ -0,0 +1,9 @@
+#Input in python
+
+name = input("Enter your name:")
+age = input("Enter your age:")
+marks = input("obtain marks:")
+
+print("Welcome",name)
+print("age =",age)
+print("your marks =",marks)
