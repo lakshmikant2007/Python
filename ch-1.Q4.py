ebccdf3eb85f6ce8ea23d7006b89408f681a5a48
Lakshmikant Kumar <lakshmikantkumar22@gmail.com>
2026-03-20 14:06:38 +0530

    Add files via upload

---
+++ ch-1.Q4.py
@@ -0,0 +1,8 @@
+#Q:- Write a program to input 2 int numbers, a and b. 
+# print True if a is grater then or equal to b.
+# it not print False.
+
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+
+print(a>=b)
